33657a137cead6db7e8555995dbf982a8c09f835
mahi9098 <gskmahesh.com@gmail.com>
2022-10-04 21:50:33 +0530

    Updated

--- hello.py
+++ hello.py
@@ -1,18 +1,9 @@
 This file contain Pythan script
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> 2451057... New Stash Updated
 Hello guys...
 I am updating in Stage branch...
 
 Modified to test stash
-<<<<<<< HEAD
 
 
 This line add foir SSH url test
 
-=======
->>>>>>> Stashed changes
->>>>>>> 2451057... New Stash Updated
